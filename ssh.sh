64a4fdc33d0f84a7db3896f16c36c9a16d7ab588
围墙 <33481440+wewall@users.noreply.github.com>
2019-08-06 18:47:12 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "请确保执行脚本用户为 Root !"
-
+user="$(id -un 2>/dev/null || true)"
+if [ "$user" != 'root' ]; then
+    echo "请确保执行脚本用户为 Root !"
+    exit 1
+fi
 setRootPubKey(){
     if [ -z "$1" ]; then
         public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6hw/BgwXg8hs8ar+V8XXOY3HxKVfM0hYPot5TWSREMYlPMHk40XR17FWEs4DFrzCGrR6auPYWay+JgCRzTcvvzwu2qMhaRIvrE5jpYW2CxNeCeZAMHd4JPedmXj5Sq5IJs6IAoM0m952NeqjnnC1NccpmmFobAyvg4OGoz4YxaapeCbpseqBcyolnyMASbei8RLbgbrCYzo++bXXeO13iimpHbttk6rLx+ONI/QygReZX0EMEJLOgpYJhP0HDz+WjekvlGJfn8bVWQK34G9RhC2UbMb71IZ12xgGLXLNJLdSqBSHKOaLOlC+b6AMgbhsUpCwZwF0FOQnmmVRNJZJD root"
