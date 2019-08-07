a1c319d5802c3506761e552a89650f409e673c33
围墙 <33481440+wewall@users.noreply.github.com>
2019-08-08 01:03:57 +0800

    disable ipv6 adr

--- bbr.sh
+++ bbr.sh
@@ -69,6 +69,9 @@ updateandconfigother(){
     echo "net.core.default_qdisc = fq_codel" >> /etc/sysctl.conf
     echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
     echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+    echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
     sysctl -p
 
 }
